be8eb0dbc943ed840974245b19118ac0e9aaa1a8
sorrowslee <sorrowslee@gmail.com>
2025-06-26 18:02:27 +0800

    Add ResourceManager for dragonBones preloading

---
+++ src/base/ResourceManager.ts
@@ -0,0 +1,37 @@
+import * as PIXI from 'pixi.js';
+
+// webpack require context for all dragonBones assets
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const dragonBonesContext = (require as any).context('../../assets', true, /dragonBones\/.*\.(json|png)$/);
+
+export class ResourceManager {
+  private static loadedGames: Record<string, boolean> = {};
+
+  public static preloadDragonBones(gameCode: string): Promise<void> {
+    if (this.loadedGames[gameCode]) {
+      return Promise.resolve();
+    }
+
+    const resources = dragonBonesContext
+      .keys()
+      .filter((key: string) => key.includes(`/${gameCode}/dragonBones/`));
+
+    if (resources.length === 0) {
+      this.loadedGames[gameCode] = true;
+      return Promise.resolve();
+    }
+
+    const loader = new PIXI.Loader();
+    resources.forEach((key: string) => {
+      const url = dragonBonesContext(key);
+      loader.add(url, url);
+    });
+
+    return new Promise(resolve => {
+      loader.load(() => {
+        this.loadedGames[gameCode] = true;
+        resolve();
+      });
+    });
+  }
+}
